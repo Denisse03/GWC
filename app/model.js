b9d0c1122dcd07216c0257baeb76328a456a3fe4
Denisse <denissehenriquez@hotmail.com>
2024-11-19 17:00:30 -0500

    changes

--- app/model.js
+++ app/model.js
@@ -1,8 +1,14 @@
-var homeContent = ` <h1>MERKAI</h1>
-      <div class="banner"></div>
+var homeContent = `  <div class="banner">
+        <h1>MERKAI</h1>
+
+        <h4>Fall 2024</h4>
+      </div>
+
+      <h3>FEATURED</h3>
 
       <div id="allInfo">
         <img
+          id="images"
           src="https://png.pngtree.com/thumb_back/fw800/background/20240327/pngtree-beauty-fashion-model-girl-wearing-sunglass-with-trendy-clothes-image_15696050.jpg"
           alt=""
         />
@@ -12,42 +18,80 @@ var homeContent = ` <h1>MERKAI</h1>
       <div id="allInfo">
         <div class="title">Food</div>
         <img
-          src="https://png.pngtree.com/thumb_back/fw800/background/20240327/pngtree-beauty-fashion-model-girl-wearing-sunglass-with-trendy-clothes-image_15696050.jpg"
+          id="images"
+          src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574"
           alt=""
         />
       </div>`;
-var infoContent = `<h1>MERKAI</h1>
+var infoContent = ` <h1>MERKAI</h1>
 
-      <h5>Best Feature</h5>
-      <div class="foodimg">
+      <div id="allInfos">
         <div class="foodOne"></div>
-        <div class="foodTwo"></div>
-        <div class="foodThree"></div>
+        <div class="infoText">
+          <h3>ChicAura</h3>
+          <p>
+            is a small boutique that is a family own business. They specialize
+            in modern clothing.
+          </p>
+          <div class="titles">27 Elmwood Ave, Unit 3B</div>
+        </div>
       </div>
 
-      <div class="foodimg">
-        <div class="foodOne"></div>
+      <div id="allInfos">
+        <div class="infoText">
+          <h3>Flavor Haven</h3>
+          <div class="titles">
+            <p>is a restaurant that specialize in various types of soups</p>
+            492 Oakridge Blvd, Apt 5
+          </div>
+        </div>
         <div class="foodTwo"></div>
-        <div class="foodThree"></div>
       </div>
 
-      <div class="foodimg">
-        <div class="foodOne"></div>
-        <div class="foodTwo"></div>
+      <div id="allInfos">
         <div class="foodThree"></div>
-      </div>`;
-var formContent = `<h1>Form</h1>
-      <form>
+        <div class="infoText">
+          <h3>Potter’s Palette</h3>
+          <p>
+            is a family own business that specialize in teaching others how to
+            make different types of pottery
+          </p>
+          <br />
+          <div class="titles">14 Pine St, Suite 102</div>
+        </div>
+      </div> `;
+var formContent = `<h2>Form</h2>
+      <form
+        action="https://formsubmit.co/77130b1b67b2d21fe45d962dfdbbfbf6"
+        method="POST"
+      >
         <label>Name</label>
-        <input type="text" />
+        <input
+          required
+          id="name"
+          type="text"
+          name="Name"
+          placeholder="Business Name"
+        />
 
-        <label>Business</label>
-        <input type="text" />
+        <label>Type of business</label>
+        <select id="business" name="Business Type" required>
+          <option>Food</option>
+          <option>Beauty & Fashion</option>
+          <option>Other</option>
+        </select>
 
-        <label>Description</label>
-        <input type="text" />
+        <label>Explain why you want to be feature</label>
 
-        <button>Submit</button>
+        <textarea
+          id="description"
+          name="message"
+          placeholder="Describe why you want to be feature"
+          required
+        >
+        </textarea>
+
+        <button type="submit">Submit</button>
       </form>`;
 
 export function pageContent(pageID) {
